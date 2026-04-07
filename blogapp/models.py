f09cc66a5a8b703503ab5c0689238d1f81604a79
MrOhidulislamOhid <ohidulus@gmail.com>
2026-04-07 16:17:23 +0600

    v_5 ( Create  modele - Category, Post, Comments and requirements.txt file

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,3 +1,5 @@
+from xml.etree.ElementTree import Comment
+
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -23,3 +25,56 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+    
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True, blank=False)
+    slug = models.SlugField(unique=True)
+    description = models.TextField(blank=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='category_creator')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+
+class Post(models.Model):
+
+    STATUS_CHOICES = [
+    ('draft', 'Draft'),
+    ('published', 'Published'),
+    ]
+
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    content = models.TextField()
+    featured_image = models.ImageField(upload_to='post_images/', blank=True, null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='posts')
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comments(models.Model):
+
+    COMMENT_CHOICES = [
+    ('pending', 'Pending'),
+    ('approved', 'Approved'),
+    ]
+
+
+
+    user_comment = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    status = models.CharField(max_length=10, choices=COMMENT_CHOICES, default='pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Comment by {self.author.username} on {self.post.title}'
