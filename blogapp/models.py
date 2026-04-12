1a77b2556ce4b17165819f06336214f61aa0eec4
MrOhidulislamOhid <ohidulus@gmail.com>
2026-04-12 19:12:24 +0600

    v_7 ( edit UserPermission model )

--- blogapp/models.py
+++ blogapp/models.py
@@ -78,3 +78,25 @@ class Comments(models.Model):
 
     def __str__(self):
         return f'Comment by {self.author.username} on {self.post.title}'
+    
+
+class UserPermission(models.Model):
+    user          = models.ForeignKey(User, on_delete=models.CASCADE, related_name="employee_user_for_permission") 
+    post          = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="post_for_permission") 
+    can_view      = models.BooleanField(default=False)
+    can_add       = models.BooleanField(default=False)
+    can_update    = models.BooleanField(default=False)
+    can_delete    = models.BooleanField(default=False)
+    created_at    = models.DateTimeField(auto_now_add=True)
+    updated_at    = models.DateTimeField(auto_now_add=False, blank=True, null=True)
+    created_by    = models.ForeignKey(User, on_delete=models.CASCADE, related_name="created_by_user") 
+    updated_by    = models.ForeignKey(User, on_delete=models.CASCADE, related_name="updated_by_user", blank=True, null=True) 
+    deleted_by    = models.ForeignKey(User, on_delete=models.CASCADE, related_name="deleted_by_user", blank=True, null=True)
+    is_active     = models.BooleanField(default=True)
+    deleted       = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = "user_permission"
+
+    def __str__(self):
+        return str(self.post)
